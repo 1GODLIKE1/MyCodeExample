0bf0b5d49fcb669d2671907be2122f1d3a1973aa
_ GODLIKE _ <gogi.san.95@mail.ru>
2021-07-25 18:24:08 +0300

    Добавьте файлы проекта.

---
+++ MyCodeExample/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace MyCodeExample
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
